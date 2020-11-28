d2ae49952f763afaa5c932eedc0602117d745e72
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2020-11-28 12:20:43 -0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e '\e[5mInstalling..\e[0m' && apt-get update && apt upgrade -y && apt install gcc && apt install g++ && apt install g++-mingw-w64-i686 && pip3 install -r requirements.txt
