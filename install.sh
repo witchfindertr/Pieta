0210a34db98f2061ef7ab0e5777dffb7214a5a75
manjoos <71747405+0xmanjoos@users.noreply.github.com>
2020-11-28 14:09:56 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo -e '\e[5mInstalling..\e[0m' && apt-get update && apt upgrade -y && apt install gcc && apt install g++ && apt install g++-mingw-w64-i686 && pip3 install -r requirements.txt
+echo -e '\e[5mInstalling..\e[0m' && apt-get update && apt upgrade -y && apt install gcc && apt install g++ && apt install g++-mingw-w64-i686 && pip3 install -r requirements.txt && chmod +x /opt/pieta
+echo "Run Pieta to get started"
